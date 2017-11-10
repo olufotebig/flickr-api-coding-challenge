1309adefd34c41a2702adc8552f5af9faee4094a
Gbekeloluwa Olufotebi <olufotebigbeke@gmail.com>
2017-11-10 10:50:13 +0100

    generate page not found component add router to project set start page component for / set detail page component for /detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { StartPageComponent } from './start-page/start-page.component';
 import { DetailPageComponent } from './detail-page/detail-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { StartPageComponent } from './start-page/start-page.component';
+
+const appRoutes: Routes = [
+  { path: '', component: StartPageComponent },
+  { path: 'detail', component: DetailPageComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     StartPageComponent,
-    DetailPageComponent
+    DetailPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
