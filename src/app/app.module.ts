7b4ac1152cffe63c0084c95241fd54ccf3a3066a
Gbekeloluwa Olufotebi <olufotebigbeke@gmail.com>
2017-11-10 13:19:07 +0100

    import HttpClientModule in app module and add to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -29,6 +30,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
