972d8f95a4fd70eda2b621d313c1b527a1c39296
Gbekeloluwa Olufotebi <olufotebigbeke@gmail.com>
2017-11-10 12:16:10 +0100

    provide search service through search page

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-start-page',
   templateUrl: './start-page.component.html',
-  styleUrls: ['./start-page.component.css']
+  styleUrls: ['./start-page.component.css'],
+  providers: [SearchService]
 })
 export class StartPageComponent implements OnInit {
 
