2f3174d4df16d9c04869b99a075a6ee61eabe3c7
Gbekeloluwa Olufotebi <olufotebigbeke@gmail.com>
2017-11-10 13:26:15 +0100

    provide photostore service

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoStoreService } from '../photo-store.service';
 import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-start-page',
   templateUrl: './start-page.component.html',
   styleUrls: ['./start-page.component.css'],
-  providers: [SearchService]
+  providers: [SearchService, PhotoStoreService]
 })
 export class StartPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
