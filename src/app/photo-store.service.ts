c017f902d49d3958cd180c37261046e0d1ed7e3c
Gbekeloluwa Olufotebi <olufotebigbeke@gmail.com>
2017-11-10 14:55:07 +0100

    add RxJS subject to photo store service

--- src/app/photo-store.service.ts
+++ src/app/photo-store.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
 import { Photo } from './photo';
+
 @Injectable()
 export class PhotoStoreService {
   dataStore: Photo[];
-  constructor() {}
+  dataStoreSubject: Subject<Photo[]>;
+  constructor() {
+    this.dataStoreSubject = new Subject();
+    this.dataStore = [];
+  }
 
   insert(photo: Photo) {
     this.dataStore.push(photo);
+    this.dataStoreSubject.next(this.dataStore);
   }
 }
