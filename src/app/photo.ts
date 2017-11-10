33405f9f556c24865b0fac90686ae800e7e288b4
Gbekeloluwa Olufotebi <olufotebigbeke@gmail.com>
2017-11-10 13:18:29 +0100

    create shape file for photo object returned from filckr api

---
+++ src/app/photo.ts
@@ -0,0 +1,20 @@
+export class Photo {
+  id: string;
+  owner: string;
+  secret: string;
+  server: string;
+  farm: number;
+  title: string;
+  ispublic: number;
+  isfriend: number;
+  isfamily: number;
+  dateupload: string;
+  datetaken: string;
+  datetakengranularity: number;
+  datetakenunknown: string;
+  ownername: string;
+  views: string;
+  url_q: string;
+  height_q: string;
+  width_q: string;
+}
